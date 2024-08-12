a2a0f46891d794f1842356e219f053722c19de5c
Ivy <ivy.gifford04@gmail.com>
2024-08-12 16:04:33 -0400

    remame the config file

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ async fn main() -> std::io::Result<()> {
 
     let settings = config::Config::builder()
         // Add in `./Settings.toml`
-        .add_source(config::File::with_name("gater_config"))
+        .add_source(config::File::with_name("ap_config"))
         // Add in settings from the environment (with a prefix of APP)
         // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
         .add_source(config::Environment::default())
