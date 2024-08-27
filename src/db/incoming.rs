5ff0495c5eaf47981fcb049dcf77b5e334a3cfb2
Ivy <ivy.gifford04@gmail.com>
2024-08-27 15:59:38 -0400

    fix silly formatting

--- src/db/incoming.rs
+++ src/db/incoming.rs
@@ -1,6 +1,9 @@
 use actix_web::web::Data;
 
-use crate::activitystream_objects::{activities::ExtendsIntransitive, core_types::ActivityStream};
+use crate::activitystream_objects::{
+    activities::{Activity, ExtendsIntransitive},
+    core_types::ActivityStream,
+};
 
 use super::conn::Conn;
 
@@ -25,84 +28,90 @@ pub async fn process_intransitive(
     intransitive: Box<ExtendsIntransitive>,
 ) {
     match *intransitive {
-        ExtendsIntransitive::ExtendsActivity(activity) => {
-            match activity.type_field {
-                crate::activitystream_objects::activities::ActivityType::Like => todo!(),
+        ExtendsIntransitive::ExtendsActivity(activity) => match activity.type_field {
+            crate::activitystream_objects::activities::ActivityType::Like => todo!(),
 
-                crate::activitystream_objects::activities::ActivityType::Create => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Delete => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Create => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Delete => todo!(),
 
-                crate::activitystream_objects::activities::ActivityType::Follow => {
-                    let from_id = activity.extends_intransitive.actor.get_id();
-                    let from = conn.get_federated_user_db_id(from_id.as_str()).await;
-                    let from = match from {
-                        Some(x) => Ok(x),
-                        None => {
-                            conn.load_federated_actor(from_id, &state.instance_domain)
-                                .await
-                        }
-                    };
-
-                    let from = match from {
-                        Ok(x) => x,
-                        Err(x) => {
-                            dbg!(x);
-                            return ();
-                        }
-                    };
-
-                    //should be the preferred username
-                    let Some(to) = activity
-                        .object
-                        .get_id()
-                        .as_str()
-                        .strip_prefix(&format!("https://{}/users/", &state.instance_domain))
-                    else {
-                        println!("invalid username: {}", activity.object.get_id().as_str());
-                        return ();
-                    };
-                    let to = conn.get_local_user_db_id(to).await;
-                    match to {
-                        Some(to) => {
-                            conn.create_follow_request(
-                                super::UserRef::Activitypub(from),
-                                super::UserRef::Local(to),
-                            )
-                            .await
-                            .unwrap();
-                        }
-                        None => return,
-                    }
-                }
-                crate::activitystream_objects::activities::ActivityType::Accept => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Reject => todo!(),
-
-                crate::activitystream_objects::activities::ActivityType::Activity => todo!(),
-
-                crate::activitystream_objects::activities::ActivityType::TentativeAccept => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Add => todo!(),
-
-                crate::activitystream_objects::activities::ActivityType::Ignore => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Join => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Leave => todo!(),
-
-                crate::activitystream_objects::activities::ActivityType::Offer => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Invite => todo!(),
-
-                crate::activitystream_objects::activities::ActivityType::TentativeReject => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Remove => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Undo => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Update => todo!(),
-                crate::activitystream_objects::activities::ActivityType::View => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Listen => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Read => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Move => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Announce => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Block => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Flag => todo!(),
-                crate::activitystream_objects::activities::ActivityType::Dislike => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Follow => {
+                handle_follow(activity, conn, state).await
             }
-        }
+            crate::activitystream_objects::activities::ActivityType::Accept => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Reject => todo!(),
+
+            crate::activitystream_objects::activities::ActivityType::Activity => todo!(),
+
+            crate::activitystream_objects::activities::ActivityType::TentativeAccept => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Add => todo!(),
+
+            crate::activitystream_objects::activities::ActivityType::Ignore => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Join => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Leave => todo!(),
+
+            crate::activitystream_objects::activities::ActivityType::Offer => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Invite => todo!(),
+
+            crate::activitystream_objects::activities::ActivityType::TentativeReject => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Remove => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Undo => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Update => todo!(),
+            crate::activitystream_objects::activities::ActivityType::View => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Listen => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Read => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Move => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Announce => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Block => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Flag => todo!(),
+            crate::activitystream_objects::activities::ActivityType::Dislike => todo!(),
+        },
         ExtendsIntransitive::Question(_) => todo!(),
+    }
+}
+
+pub async fn handle_follow(
+    activity: Activity,
+    conn: Data<Box<dyn Conn + Sync>>,
+    state: Data<crate::config::Config>,
+) {
+    let from_id = activity.extends_intransitive.actor.get_id();
+    let from = conn.get_federated_user_db_id(from_id.as_str()).await;
+    let from = match from {
+        Some(x) => Ok(x),
+        None => {
+            conn.load_federated_actor(from_id, &state.instance_domain)
+                .await
+        }
+    };
+
+    let from = match from {
+        Ok(x) => x,
+        Err(x) => {
+            dbg!(x);
+            return ();
+        }
+    };
+
+    //should be the preferred username
+    let Some(to) = activity
+        .object
+        .get_id()
+        .as_str()
+        .strip_prefix(&format!("https://{}/users/", &state.instance_domain))
+    else {
+        println!("invalid username: {}", activity.object.get_id().as_str());
+        return ();
+    };
+    let to = conn.get_local_user_db_id(to).await;
+    match to {
+        Some(to) => {
+            conn.create_follow_request(
+                super::UserRef::Activitypub(from),
+                super::UserRef::Local(to),
+            )
+            .await
+            .unwrap();
+        }
+        None => return,
     }
 }
