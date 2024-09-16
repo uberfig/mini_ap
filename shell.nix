5ac3a17984ead873be487775dd03c343544a44c4
Ivy <ivy.gifford04@gmail.com>
2024-09-16 10:52:33 -0400

    fix shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,12 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   # Get dependencies from the main package
-  inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
+  # inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
+  buildInputs = with pkgs; [
+    openssl
+    openssl.dev
+    postgresql
+    pkg-config
+  ];
+  # buildInputs = [ (pkgs.callPackage ./default.nix { }) ];
 }
