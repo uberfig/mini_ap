03bbbd53c3f8b4cffa15c99cdf76931b52e5de91
uberfig <ivy.gifford04@gmail.com>
2024-09-18 13:53:22 -0400

    finish pem for key

--- src/cryptography/private_key.rs
+++ src/cryptography/private_key.rs
@@ -1,5 +1,6 @@
-use ed25519::pkcs8::{spki::der::pem::LineEnding, EncodePublicKey};
+use ed25519::pkcs8::{spki::der::pem::LineEnding, EncodePublicKey, EncodePrivateKey};
 use ed25519_dalek::SigningKey;
+
 use rand::rngs::OsRng;
 
 // ed25519_dalek::pkcs8::spki::der::EncodePem;
@@ -31,14 +32,19 @@ impl PrivateKey for AlgorithmsPrivateKey {
     fn private_key_pem(&self) -> String {
         match self {
             AlgorithmsPrivateKey::Ed25519(signing_key) => {
-                let val = signing_key.verifying_key();
-                let test = val.to_public_key_pem(LineEnding::default());
-                test.expect("for some reason the verifying key failed to encode as a public key pem. please send this along to the bayou project for further investigation")
+                let test = signing_key.to_pkcs8_pem(LineEnding::default());
+                test.expect("for some reason the private key failed to encode").to_string()
             }
         }
     }
 
     fn public_key_pem(&self) -> String {
-        todo!()
+        match self {
+            AlgorithmsPrivateKey::Ed25519(signing_key) => {
+                let val = signing_key.verifying_key();
+                let test = val.to_public_key_pem(LineEnding::default());
+                test.expect("for some reason the verifying key failed to encode as a public key pem. please send this along to the bayou project for further investigation")
+            }
+        }
     }
 }
