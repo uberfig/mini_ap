adb454d21c7eb068f49a4ff689ac3b5945c67994
uberfig <ivy.gifford04@gmail.com>
2024-09-20 00:32:05 -0400

    Update instance_discovery.rs

--- src/api/versia_api/instance_discovery.rs
+++ src/api/versia_api/instance_discovery.rs
@@ -1,17 +1,11 @@
 use crate::db::conn::Conn;
-use actix_web::{get, web::Data, HttpRequest, HttpResponse, Result};
+use actix_web::{get, web::Data, HttpResponse, Result};
 
 #[get("/.well-known/versia")]
 pub async fn versia_metadata(
     state: Data<crate::config::Config>,
     conn: Data<Box<dyn Conn + Sync>>,
-    request: HttpRequest,
 ) -> Result<HttpResponse> {
-    let request_headers = request.headers();
-    let _accept = request_headers
-        .get("Accept")
-        .map(|x| String::from_utf8(x.as_bytes().to_vec()));
-
     let metadata = conn
         .get_versia_instance_metadata(&state.instance_domain)
         .await;
