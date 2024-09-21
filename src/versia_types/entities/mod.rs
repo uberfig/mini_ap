702187af5d60de25af06be35bcac0c99efdc6f85
Ivy <ivy.gifford04@gmail.com>
2024-09-21 13:36:02 -0400

    Update mod.rs

--- src/versia_types/entities/mod.rs
+++ src/versia_types/entities/mod.rs
@@ -1,11 +1,3 @@
-use change_follow::ChangeFollowing;
-use follow_response::FollowResponse;
-use group::Group;
-use instance_metadata::InstanceMetadata;
-use notes::Note;
-use serde::{Deserialize, Serialize};
-use user::User;
-
 pub mod change_follow;
 pub mod delete;
 pub mod follow_response;
@@ -14,15 +6,3 @@ pub mod instance_metadata;
 pub mod notes;
 pub mod public_key;
 pub mod user;
-
-/// array of a contigous type of entity
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum EntityArray {
-    FollowResponse(Vec<FollowResponse>),
-    ChangeFollowing(Vec<ChangeFollowing>),
-    Group(Vec<Group>),
-    InstanceMetadata(Vec<InstanceMetadata>),
-    Note(Vec<Note>),
-    User(Vec<User>),
-}
